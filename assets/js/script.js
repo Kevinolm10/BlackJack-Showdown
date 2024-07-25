31d033521141e9879b1ee010603e698ab659fd0a
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-07-25 21:29:46 +0000

    Add to startgame function and getamount to be able to display cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,15 +23,65 @@ function buildCardDeck() {
         }
 
     }
-    console.log(cardDeck)
+    //console.log(cardDeck)
 }
 
 function mixCards() {
     for (let i = 0; i < cardDeck.length; i++) {
         let f = Math.floor(Math.random() * cardDeck.length); // Chooses random card from 1-52
+        let card = cardDeck[i];
+        cardDeck[i] = cardDeck[f];
+        cardDeck[f] = card;
     }
+    console.log(cardDeck);
+}
+
+function getImagesSrc(images) {
+    let image = "../images/"; // Update this to the correct path of your card images
+    return `${image}${images}.png`;
 }
 
 function startGame() {
+    flip = cardDeck.pop();
+    dealerPoint += getAmount(flip);
+    dealerAces += scanAce(flip);
+    console.log(flip);
+    //console.log(cardDeck);
+    while (dealerPoint < 17) {
+        flip = cardDeck.pop();
+        dealerPoint += getAmount(flip);
+        dealerAces += scanAce(flip);
+        console.log(flip);
 
+        if (dealerPoint > 21 && dealerAces > 0) {
+            dealerPoint -= 10;
+            dealerAces -= 1;
+        }
+
+
+        let cardImage = document.createElement("img");
+        image.src = getImageSrc(flip);
+        document.getElementById("dealer-cards").appendChild(image);
+    }
+
+}
+
+function getAmount(images) {
+    let data = images.split("-");
+    let amount = data[0];
+
+    if (isNaN(amount)) {
+        if (amount === "a") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(amount);
+}
+
+function scanAce(images) {
+    if (images[0] === "a") {
+        return 1;
+    }
+    return 0;
 }
