c5441d1c7e76c87f0b4d00f174c56425a344b5ab
Kevin Ölmheden <Kevinolm10@gmail.com>
2024-07-28 09:05:12 +0000

    Redo the startGame function for simpler code and to debug the cards not showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,9 @@ let dealerAces = 0; // dealer and your ace count
 let playerAces = 0;
 let flip; // hidden card in the beginning
 let cardDeck = [];
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "j", "q", "k"];
 const types = ["c", "d", "h", "s"];
+
 
 let goBtn = true; //allows to go if you are under 21 points
 
@@ -36,53 +37,37 @@ function mixCards() {
     console.log(cardDeck);
 }
 
-// Function to get the img source
-function getImageSrc(card) {
-    return `/workspace/BlackJack-Showdown/assets/images${values} + "-" + ${types} + .png`;
-}
-
 //function for when the game starts
 function startGame() {
     flip = cardDeck.pop();
     dealerPoint += getAmount(flip);
     dealerAces += scanAce(flip);
-    console.log(flip);
 
     while (dealerPoint < 17) {
-        flip = cardDeck.pop();
-        dealerPoint += getAmount(flip);
-        dealerAces += scanAce(flip);
-        console.log(flip);
-
-        if (dealerPoint > 21 && dealerAces > 0) {
-            dealerPoint -= 10;
-            dealerAces -= 1;
-        }
-
-        // for dealer cards to show
-        let cardImage = document.createElement("img");
-        cardImage.src = getImageSrc(flip);
-
-        let dealerCardsContainer = document.getElementsByClassName("dealers-cards");
-        if (dealerCardsContainer.length > 0) {
-            dealerCardsContainer[0].appendChild(cardImage);
-        }
-        console.log(dealerPoint);
+        let imageImg = document.createElement("img");
+        let image = cardDeck.pop();
+        imageImg.src = "./images/" + image + ".png";
+        dealerPoint += getAmount(image);
+        dealerAces += scanAce(image);
+        document.getElementsByClassName("dealers-cards")[0].append(imageImg);
     }
-    //for player cards to show up
+    console.log(dealerPoint);
+
     for (let i = 0; i < 2; i++) {
-        let cardImage = document.createElement("img");
-        cardImage.src = getImageSrc(flip);
-
-        let playerCardsContainer = document.getElementsByClassName("players-cards");
-        if (playerCardsContainer.length > 0) {
-            playerCardsContainer[0].appendChild(cardImage);
-        }
-        console.log(dealerPoint);
+        let imageImg = document.createElement("img");
+        let image = cardDeck.pop();
+        imageImg.src = "./images/" + image + ".png";
+        playerPoint += getAmount(image);
+        playerAces += scanAce(image);
+        document.getElementsByClassName("players-cards")[0].append(imageImg);
     }
 
-    function getAmount(images) {
-        let data = images.split("-");
+    console.log(playerPoint)
+    document.getElementsByClassName("go").addEventListener("click", go);
+    document.getElementsByClassName("stay").addEventListener("click", stay);
+
+    function getAmount(image) {
+        let data = image.split("-");
         let amount = data[0];
 
         if (isNaN(amount)) {
@@ -94,11 +79,10 @@ function startGame() {
         return parseInt(amount);
     }
 
-    function scanAce(images) {
-        if (images[0] === "a") {
+    function scanAce(image) {
+        if (image[0] === "a") {
             return 1;
         }
         return 0;
     }
-
 }
